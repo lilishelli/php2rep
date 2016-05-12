61cf51dad69a09a0f4c9689a88e2402a97a7d645
vityanich <vityanich22@gmail.com>
2016-05-12 13:58:29 +0400

    fix

--- MyClass.php
+++ MyClass.php
@@ -3,10 +3,6 @@ class MyClass {
 
     public function power($x, $y)
     {
-        return pow($x, $x);
-    }
-	public function mul($x, $y)
-    {
-        return $x*$y;
+        return pow($x, $y);
     }
 }
